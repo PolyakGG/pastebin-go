c3abc09682ff0b250453c24521cd8f9b4711d492
PolyakGG <separik8@gmail.com>
2024-10-05 22:37:14 +0400

    added service layer for html page

--- internal/repository/sqlite.go
+++ internal/repository/sqlite.go
@@ -2,20 +2,21 @@ package repository
 
 import (
 	"database/sql"
+	"errors"
 	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"pastebin-go/internal/models"
 )
 
-type PasteRepository struct {
-	DB *sql.DB
-}
+type PasteRepository struct { // - Здесь мы организуем доступ к БД в приложении
+	DB *sql.DB // - Используем указатель чтобы не копировать всего объекта базы данных при передаче его в функции или методы
+} // - изменять соединение с базой данных в одном месте и видеть эти изменения во всем приложении
 
 func NewPasteRepository(dbPath string) (*PasteRepository, error) {
 	log.Printf("Connecting to database at path: %s", dbPath)
 	db, err := sql.Open("sqlite3", dbPath)
 	if err != nil {
-		return nil, err
+		return nil, errors.New("не удалось подключиться к базе данных")
 	}
 	return &PasteRepository{DB: db}, nil
 }
@@ -38,12 +39,33 @@ func (r *PasteRepository) Create(paste *models.Paste) (int, error) {
 	return int(id), nil
 }
 
-func (r *PasteRepository) GetById(id int) (*models.Paste, error) {
+func (r *PasteRepository) GetById(id int) (*models.Paste, error) { // С помощью запроса по id получаем данные
 	paste := &models.Paste{}
-	err := r.DB.QueryRow("SELECT id, title,content, created_at FROM pastes WHERE id=?", id).
-		Scan(&paste.ID, &paste.Title, &paste.Content, &paste.CreatedAt)
+	err := r.DB.QueryRow("SELECT id, title,content, created_at FROM pastes WHERE id=?", id). //[.]- это Метод-чейнинг(цепочка вызова методов)
+													Scan(&paste.ID, &paste.Title, &paste.Content, &paste.CreatedAt) //  метод Scan автоматически заполняет соответствующие поля в структуре paste.
 	if err != nil {
 		return nil, err
 	}
 	return paste, nil
+}
+
+// Сервисный слой
+
+func (r *PasteRepository) GetAll() ([]models.Paste, error) {
+	rows, err := r.DB.Query("SELECT id, title, content, created_at FROM pastes")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var pastes []models.Paste
+	for rows.Next() {
+		var paste models.Paste
+		if err := rows.Scan(&paste.ID, &paste.Title, &paste.Content, &paste.CreatedAt); err != nil {
+			return nil, err
+		}
+		pastes = append(pastes, paste)
+	}
+
+	return pastes, nil
 }
